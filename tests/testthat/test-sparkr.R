8eea11531365e0f0811e641a3072f59704954771
Liam Surry <erethizon3@gmail.com>
2024-11-01 09:59:13 -0400

    Updated tests

--- tests/testthat/test-sparkr.R
+++ tests/testthat/test-sparkr.R
@@ -1,34 +1,9 @@
 #requires you to run this command first: usethis::use_testthat(3)
 library(testthat)
 
-test_that("sparkr_works_TRUE_no_cat", {
+test_that("sparkr_works_TRUE", {
   result <- sparkr(TRUE)
-  expect_true(is.character(result))
-})
-
-test_that("sparkr_works_TRUE_Bad", {
-  result <- sparkr(TRUE, category = "Bad")
-  expect_true(is.character(result))
-})
-
-test_that("sparkr_works_TRUE_Wellness", {
-  result <- sparkr(TRUE, category = "Wellness")
-  expect_true(is.character(result))
-})
-
-test_that("sparkr_works_TRUE_Physical", {
-  result <- sparkr(TRUE, category = "Physical")
-  expect_true(is.character(result))
-})
-
-test_that("sparkr_works_TRUE_Creative", {
-  result <- sparkr(TRUE, category = "Creative")
-  expect_true(is.character(result))
-})
-
-test_that("sparkr_works_TRUE_Social", {
-  result <- sparkr(TRUE, category = "Social")
-  expect_true(is.character(result))
+  expect_true(result %in% sparkr_dataset$ideas)
 })
 
 test_that("sparkr_works_FALSE", {
